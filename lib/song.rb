905e9c8dbc73b2dd898f7f18fd970f3e4737fd4a
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-21 15:27:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     return song
   end
 
-  def self.find_by_name(song)
+  def self.find_by_name(name)
     @@all.each do |song|
-      if (song.name == name)
+      if (song.names == name)
         return song
       end
     end
